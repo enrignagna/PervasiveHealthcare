b840719c791526f6e826cf02192b9dfd1db92499
Enrico Gnagnarella <enrico.gnagnarella@studio.unibo.it>
2021-03-05 11:32:01 +0100

    Update build.gradle.kts with some functionality of Jacoco. New code for scala version.

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,6 +8,7 @@ plugins {
 group = "io.github.enrignagna"
 
 
+
 repositories {
     jcenter()
     mavenLocal()
@@ -15,37 +16,36 @@ repositories {
 }
 
 dependencies {
-    //TODO: Remove imperative part of scala version
     val scalaVersion = "2.12"
+    implementation("org.scala-lang:scala-library:$scalaVersion")
 
+    //TODO: Remove imperative part of scala version
+    //implementation("ch.epfl.scala:scalafix-core_2.12:_")
     implementation("org.mongodb.scala:mongo-scala-driver_$scalaVersion:_")
-    //implementation("io.spray:spray-json_$scalaVersion:_")
+    implementation("io.spray:spray-json_$scalaVersion:_")
     implementation("com.lightbend.akka:akka-stream-alpakka-mongodb_$scalaVersion:_")
     implementation("com.typesafe.akka:akka-stream_$scalaVersion:_")
-    implementation("com.typesafe.akka:akka-actor-typed_$scalaVersion:_")
     implementation("com.typesafe.akka:akka-slf4j_$scalaVersion:_")
-    implementation("com.typesafe.akka:akka-http_$scalaVersion:_")
-    implementation("com.typesafe.akka:akka-http-spray-json_$scalaVersion:_")
-    //implementation("ch.epfl.scala:scalafix-core_2.12:_")
-    //implementation("org.scala-lang:scala-library:2.12")
 
-    testImplementation("junit:junit:_")
+
+    testImplementation("junit:junit:4.13")
     testImplementation("org.scalatest:scalatest_$scalaVersion:_")
     testImplementation("org.scalatestplus:junit-4-12_$scalaVersion:_")
-    testImplementation("com.typesafe.akka:akka-http-testkit_$scalaVersion:_")
-    testImplementation("com.typesafe.akka:akka-actor-testkit-typed_$scalaVersion:_")
-
 
     testRuntimeOnly("org.scala-lang.modules:scala-xml_$scalaVersion:_")
 }
 
+
 tasks.jacocoTestReport {
     reports {
+        xml.isEnabled = true
+        xml.destination = file("${buildDir}/reports/jacoco/report.xml")
         html.isEnabled = true
-        html.destination = file("${buildDir}/jacocoHtml")
+        html.destination = file("${buildDir}/reports/jacoco/jacocoHtml")
     }
 }
 
 gitSemVer {
     version = computeGitSemVer()
 }
+
