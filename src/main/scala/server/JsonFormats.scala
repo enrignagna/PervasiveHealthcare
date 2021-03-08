5bcf32bb5ad4d7d3a45583104405eb4b014b4e54
scumatteo <matteo.scucchia@studio.unibo.it>
2021-03-08 15:37:07 +0100

    Added a first trivial implementation of Akka Http server

---
+++ src/main/scala/server/JsonFormats.scala
@@ -0,0 +1,63 @@
+/*
+ *
+ *  * Copyright (c) 2021.  Ylenia Battistini, Enrico Gnagnarella, Matteo Scucchia
+ *  *
+ *  *                              Licensed under the Apache License, Version 2.0 (the "License");
+ *  *                              you may not use this file except in compliance with the License.
+ *  *                              You may obtain a copy of the License at
+ *  *
+ *  *                                  http://www.apache.org/licenses/LICENSE-2.0
+ *  *
+ *  *                              Unless required by applicable law or agreed to in writing, software
+ *  *                              distributed under the License is distributed on an "AS IS" BASIS,
+ *  *                              WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  *                              See the License for the specific language governing permissions and
+ *  *                              limitations under the License.
+ *
+ */
+
+package server
+
+import domainmodel.professionalfigure.Specialization._
+import domainmodel.professionalfigure.{DoctorID, Specialization, Surgeon}
+import server.controllers.AdministratorController.CreateSurgeonResponse
+import spray.json.{DeserializationException, JsNumber, JsString, JsValue, RootJsonFormat}
+
+object JsonFormats  {
+  // import the default encoders for primitive types (Int, String, Lists etc)
+  import spray.json.DefaultJsonProtocol._
+
+  implicit val doctorIDJsonFormat: RootJsonFormat[DoctorID] = jsonFormat1(DoctorID)
+
+  //With ID, i prefer
+
+  class EnumJsonConverter[T <: scala.Enumeration](enu: T) extends RootJsonFormat[T#Value] {
+    override def write(obj: T#Value): JsValue = JsNumber(obj.id)
+
+    override def read(json: JsValue): T#Value = {
+      json match {
+        case JsNumber(txt) => enu.withName(enu.values.toList(txt.intValue()).toString)
+        case somethingElse => throw DeserializationException(s"Expected a value from enum $enu instead of $somethingElse")
+      }
+    }
+  }
+
+  //With string
+
+  /*
+  class EnumJsonConverter[T <: scala.Enumeration](enu: T) extends RootJsonFormat[T#Value] {
+    override def write(obj: T#Value): JsValue = JsString(obj.toString)
+
+    override def read(json: JsValue): T#Value = {
+      json match {
+        case JsString(txt) => enu.withName(txt)
+        case somethingElse => throw DeserializationException(s"Expected a value from enum $enu instead of $somethingElse")
+      }
+    }
+  }
+  */
+
+  implicit val enumConverter: EnumJsonConverter[Specialization.type] = new EnumJsonConverter(Specialization)
+  implicit val surgeonJsonFormat: RootJsonFormat[Surgeon] = jsonFormat6(Surgeon)
+
+}
---
+++ src/main/scala/server/routes/AdministratorRoutes.scala
@@ -0,0 +1,63 @@
+/*
+ *
+ *  * Copyright (c) 2021.  Ylenia Battistini, Enrico Gnagnarella, Matteo Scucchia
+ *  *
+ *  *                              Licensed under the Apache License, Version 2.0 (the "License");
+ *  *                              you may not use this file except in compliance with the License.
+ *  *                              You may obtain a copy of the License at
+ *  *
+ *  *                                  http://www.apache.org/licenses/LICENSE-2.0
+ *  *
+ *  *                              Unless required by applicable law or agreed to in writing, software
+ *  *                              distributed under the License is distributed on an "AS IS" BASIS,
+ *  *                              WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  *                              See the License for the specific language governing permissions and
+ *  *                              limitations under the License.
+ *
+ */
+
+package server.routes
+
+import java.time
+import java.time.Duration
+
+import akka.actor.typed.scaladsl.AskPattern.{Askable, schedulerFromActorSystem}
+import akka.actor.typed.{ActorRef, ActorSystem}
+import akka.http.scaladsl.server.Directives.{complete, get, pathPrefix}
+import akka.http.scaladsl.server.Directives._
+import akka.http.scaladsl.model.StatusCodes
+import akka.http.scaladsl.server.Route
+import akka.util.Timeout
+import domainmodel.professionalfigure.Surgeon
+import server.controllers.AdministratorController
+import server.controllers.AdministratorController.{CreateSurgeon, CreateSurgeonResponse}
+
+import scala.concurrent.Future
+import scala.concurrent.duration.DurationInt
+
+class AdministratorRoutes(administratorController: ActorRef[AdministratorController.Command])(implicit val system: ActorSystem[_]) {
+
+  private implicit val timeout = Timeout(500.milliseconds)
+
+  import akka.http.scaladsl.marshallers.sprayjson.SprayJsonSupport._
+  import server.JsonFormats._
+
+  def createSurgeon(surgeon: Surgeon): Future[CreateSurgeonResponse] =
+    administratorController.ask(CreateSurgeon(surgeon, _))
+
+  val administratorRoutes: Route =
+  pathPrefix("surgeon") {
+    concat(
+      pathEnd {
+        concat(
+          post {
+            entity(as[Surgeon]) { surgeon =>
+              onSuccess(createSurgeon(surgeon)) { response =>
+                complete(response.maybeUser)
+              }
+            }
+          })
+      })
+  }
+
+}
